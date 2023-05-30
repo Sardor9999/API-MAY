4f313e85a83f5914ab6066efb869541b3ba69b83
Sardor9999 <sardoryuldashov9999@gmail.com>
2023-05-30 23:54:31 +0300

    a

---
+++ config/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+# from rest_framework.routers import DefaultRouter
+# from api.views import CustomListCreatAPIView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('api.urls')),
+    path('api-auth/', include('rest_framework.urls'))
+]
